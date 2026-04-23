0f8131445864b443bc482d09e28b0f7ed3d72473
KasumiAyaka <kasumi@flab.phys.nagoya-u.ac.jp>
2026-04-23 22:46:04 +0900

    add overwrite mode

--- Merge_momchain/Merge_momchain.cpp
+++ Merge_momchain/Merge_momchain.cpp
@@ -4,13 +4,17 @@
 std::vector<Momentum_recon::Event_information> merge_momch(std::vector<Momentum_recon::Event_information>& momch0, std::vector<Momentum_recon::Event_information>& momch1);
 
 int main(int argc, char** argv) {
-	if (argc != 4) {
-		fprintf(stderr, "usage:file-in-momchain file-in-momchain file-out-momchain\n");
+	if (argc < 4) {
+		fprintf(stderr, "usage:file-in-momchain file-in-momchain file-out-momchain [overwrite mode]\nIf you set the overwrite mode flag to 1, \nany existing event will be overwritten with the values from the second file.\nIf not specified, the values from the first file will be output.\n");
 		exit(1);
 	}
 	std::string file_in_momch0 = argv[1];
 	std::string file_in_momch1 = argv[2];
 	std::string file_out_momch = argv[3];
+	int mode = -1;
+	if (argc > 4) {
+		mode = std::stoi(argv[4]);
+	}
 
 	std::vector<Momentum_recon::Event_information> momch0, momch1;
 	momch0 = Momentum_recon::Read_Event_information_extension(file_in_momch0);
@@ -24,25 +28,63 @@ int main(int argc, char** argv) {
 
 std::vector<Momentum_recon::Event_information> merge_momch(std::vector<Momentum_recon::Event_information>& momch0, std::vector<Momentum_recon::Event_information>& momch1) {
 	std::map<int, Momentum_recon::Event_information> m_momch;
-	
-	std::cout << "momch1 size: " << momch0.size() << std::endl;
-	std::cout << "momch2 size: " << momch1.size() << std::endl;
+
 
 	for (auto& ev : momch0) {
-			m_momch.insert(std::make_pair(ev.groupid, ev));
+		m_momch.insert(std::make_pair(ev.groupid, ev));
 	}
 
 	for (auto& ev : momch1) {
-			auto res = m_momch.find(ev.groupid);
-			if (res==m_momch.end()) {
-				//fprintf(stderr, "same chain was input groupid=%d chainid=%d\n", ev.groupid, c.chainid);
-				std::cout << "not existed event:" << ev.groupid <<  std::endl;
-				m_momch.insert(std::make_pair(ev.groupid, ev));
-			}
+		auto res = m_momch.find(ev.groupid);
+		if (res == m_momch.end()) {
+			//fprintf(stderr, "same chain was input groupid=%d chainid=%d\n", ev.groupid, c.chainid);
+			std::cout << "not existed event:" << ev.groupid << std::endl;
+			m_momch.insert(std::make_pair(ev.groupid, ev));
+		}
 
 	}
-	
+
+	std::cout << "momch1 size: " << momch0.size() << std::endl;
+	std::cout << "momch2 size: " << momch1.size() << std::endl;
 	std::cout << "merged size: " << m_momch.size() << std::endl;
+
+
+	std::vector<Momentum_recon::Event_information > ret;
+
+	for (auto itr = m_momch.begin(); itr != m_momch.end(); itr++) {
+		ret.push_back(itr->second);
+	}
+	return ret;
+
+}
+
+std::vector<Momentum_recon::Event_information> merge_momch_overwrite(std::vector<Momentum_recon::Event_information>& momch0, std::vector<Momentum_recon::Event_information>& momch1) {
+	std::map<int, Momentum_recon::Event_information> m_momch;
+
+
+	for (auto& ev : momch0) {
+		m_momch.insert(std::make_pair(ev.groupid, ev));
+	}
+
+	int cnt = 0;
+	for (auto& ev : momch1) {
+		auto res = m_momch.find(ev.groupid);
+		if (res == m_momch.end()) {
+			std::cout << "not existed event:" << ev.groupid << std::endl;
+			m_momch.insert(std::make_pair(ev.groupid, ev));
+		}
+		else {// duplicated
+			std::cout << "overwrite :" << ev.groupid << std::endl;
+			res->second = ev;
+			cnt++;
+		}
+
+	}
+
+	std::cout << "momch1 size: " << momch0.size() << std::endl;
+	std::cout << "momch2 size: " << momch1.size() << std::endl;
+	std::cout << "merged size: " << m_momch.size() << std::endl;
+	std::cout << "overwrite size: " << cnt << std::endl;
 
 
 	std::vector<Momentum_recon::Event_information > ret;
